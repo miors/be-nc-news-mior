a758cc91468b0be6c231401ed478e7c796dc9d6b
miors <mior.sufian@googlemail.com>
2024-05-05 17:11:12 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const {
   handleServerErrors,
 } = require("./errors/index.js");
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
